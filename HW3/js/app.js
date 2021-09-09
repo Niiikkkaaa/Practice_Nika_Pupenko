599849a3d2d357f54997e9527059f8ca0d352664
nnikaaa <nikkollnikki@gmail.com>
2021-09-09 14:03:58 +0300

    HW3 fixed

--- HW3/js/app.js
+++ HW3/js/app.js
@@ -1,14 +1,15 @@
-Array.prototype.myFilter = function(callback, args = this) {
+Array.prototype.myFilter = function(callback, thisArr) {
   let result = [];
 
-  for (let i = 0; i < args.length; i++) {
-    if (callback.call(args[i], i, args)) {
-      result.push(args[i]);
+  for (let i = 0; i < this.length; i++) {
+    if (callback.call(thisArr, this[i], i, this)) {
+      result.push(this[i]);
     }
   }
 
   return result;
 } 
+
 
 function createDebounceFunction(callback, ms) {
   let timeout;
